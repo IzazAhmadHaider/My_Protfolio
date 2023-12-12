a44fad2c9409756f54a11c0cb2a5e5e693616894
izaz ahmad <izazahmadhaider0@gmail.com>
2023-12-12 14:33:32 +0500

    added ui changes

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,9 @@
 // pages/index.js
 import React from 'react';
-import Navbar from './components/Navbar';
 
 const Home = () => {
   return (
     <main className="h-[210vh]">
-      <Navbar />
-      {/* Other content goes here */}
     </main>
   );
 };
