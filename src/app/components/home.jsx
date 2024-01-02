d5e18f952761b218d663ffb09aa39705556ef15c
izaz ahmad <izazahmadhaider0@gmail.com>
2024-01-02 19:58:08 +0500

    added some functionality to home page

--- src/app/components/home.jsx
+++ src/app/components/home.jsx
@@ -1,9 +1,44 @@
+import React,{ useState,useEffect } from "react";
 function Home1() {
+  const [text, setText] = useState('');
+  const fullText = 'Frontend Web Developer | ReactJs';
+  useEffect(() => {
+    let currentIndex = 0;
+    const intervalId = setInterval(() => {
+      setText(fullText.slice(0, currentIndex));
+      currentIndex++;
+      if (currentIndex > fullText.length) {
+        clearInterval(intervalId);
+      }
+    }, 100); // Adjust the interval for typing speed
+
+    return () => {
+      clearInterval(intervalId);
+    };
+  }, []);
+
   return (
-    <div>
-      i am home1 component hellllllllllllooooooooooooooooooooo
-    </div>
-  )
+    <>
+      <div className="flex items-center justify-between bg-yellow-300 ">
+        <div className=" pl-28 font">
+          <p className="text-4xl text-[#219ebc] ">Hello!</p>
+          <h6 className="text-3xl text-[#219ebc]">I'm Izaz Ahmad</h6>
+          <h6 className="typewriter text-[#219ebc]">{text} </h6>
+        </div>
+        <div className="min-h-screen flex flex-col items-end pr-48">
+          <div className="flex-grow bg-yellow-300 flex items-center justify-center">
+            <div
+              className="relative  w-72 h-72 bg-yellow-200 border-2 border-[#219ebc] custom-spin-animation rounded-full"
+              style={{
+                clipPath: "circle(90% at 0% 20%)",
+              }}
+            ></div>
+            <img className="absolute w-64" src="mypic.png" alt="" />
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
-export default Home1
+export default Home1;
--- src/app/page.js
+++ src/app/page.js
@@ -3,6 +3,7 @@
 import React, { useEffect, useState } from "react";
 import { Link as ScrollLink, animateScroll as scroll } from "react-scroll";
 import Home1 from "./components/home"
+import { IconBrandFacebook, IconBrandInstagram, IconBrandLinkedin, IconBrandX } from "@tabler/icons-react";
 
 const Home = () => {
   const [showNavbar, setShowNavbar] = useState(false);
@@ -27,6 +28,23 @@ const Home = () => {
 
   return (
     <div className="h-auto">
+       <div className="bg-transparent flex items-center justify-between w-full fixed  ">
+       <div className="flex items-center pt-3 pl-3">
+            <div
+              className="relative  w-7 h-7 bg-yellow-200 border-2 border-[#219ebc] custom-spin-animation rounded-full"
+              style={{
+                clipPath: "circle(90% at 0% 20%)",
+              }}
+            ></div>
+            <img className="absolute w-6 pl-[3px] " src="mypic.png" alt="" />
+            <p className="text-[#219ebc] text-lg font">&nbsp;&nbsp;Izaz Ahmad</p></div>
+          <div className="flex items-end pr-3 space-x-2">
+            <IconBrandInstagram color="#219ebc" size={20} className="hover:scale-110 cursor-pointer"/>
+            <IconBrandFacebook  color="#219ebc" size={20} className="hover:scale-110 cursor-pointer"/>
+            <IconBrandLinkedin  color="#219ebc" size={20} className="hover:scale-110 cursor-pointer"/>
+            <IconBrandX         color="#219ebc" size={20} className="hover:scale-110 cursor-pointer"/>
+            </div>
+          </div>
       <div
         className={`${
           showNavbar
@@ -34,7 +52,7 @@ const Home = () => {
             : "hidden"
         }`}
       >
-        <nav className="flex justify-between items-center text-black font-serif font-semibold mx-3">
+        <nav className="flex justify-between items-center text-black font-serif font-semibold mx-3 z-0">
           <ScrollLink to="home" activeClass="hover:bg-gradient-to-r from-green-400 via-green-100 to-green-400 p-1 hover:rounded-2xl" className="cursor-pointer hover:bg-gradient-to-r from-green-400 via-green-100 to-green-400 p-1 hover:rounded-2xl" smooth={true} duration={500}>
             Home
           </ScrollLink>
@@ -53,7 +71,7 @@ const Home = () => {
         </nav>
       </div>
 
-      <section className="h-[100vh] bg-zinc-500  font-semibold font-serif" id="home">
+      <section className="h-[100vh] bg-yellow-300  font-semibold font-serif" id="home">
         <Home1/>
       </section>
       <section className="h-[100vh] bg-violet-200  font-semibold font-serif" id="skills">
